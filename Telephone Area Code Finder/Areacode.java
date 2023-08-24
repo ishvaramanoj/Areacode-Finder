80e6946637b2dc0f9f0dba50c831ca3612aea7cf
Ishvara Manoj <141846526+ishvaramanoj@users.noreply.github.com>
2023-08-24 19:47:10 +0530

    Add files via upload

---
+++ Telephone Area Code Finder/Areacode.java
@@ -0,0 +1,107 @@
+import java.util.Scanner;
+class Areacode{
+	public static void main(String[]args){
+		
+	Scanner scan = new Scanner(System.in);
+		
+	System.out.println(" *** WELCOME TO THE TELEPHONE AREA CODE FINDER - SRI LANKA *** ");	
+	
+	System.out.println("");
+	System.out.println("      Please refer the following regions of the country ");	
+		
+	System.out.println("");	
+	System.out.println("");
+		
+	System.out.println("Ampara         : 1"+ " "+" "+ " "+" "+ " "+" "+                                   "Kegalle      : 16");	                     
+	System.out.println("Awissawella    : 2"+ " "+" "+" "+" "+" "+" "+                                     "Kurunegala   : 17");	                     
+	System.out.println("Anuradhapura   : 3"+" "+" "+" "+" "+" "+" "+                                      "Mannar       : 18");	
+	System.out.println("Badulla        : 4"+" "+" "+" "+" "+" "+" "+                                      "Matale       : 19");	
+	System.out.println("Bandarawela    : 5"+" "+" "+" "+" "+" "+" "+                                      "Matara       : 20");	
+	System.out.println("Batticaloa     : 6"+" "+" "+" "+" "+" "+" "+                                      "Nawalapitiya : 21");	
+	System.out.println("Chilaw         : 7"+" "+" "+" "+" "+" "+" "+                                      "Negombo      : 22");	
+	System.out.println("Colombo        : 8"+" "+" "+" "+" "+" "+" "+                                      "NuwaraEliya  : 23");	
+	System.out.println("Galle          : 9"+" "+" "+" "+" "+" "+" "+                                      "Panadura     : 24");	
+	System.out.println("Gampaha        : 10"+" "+" "+" "+" "+" "+                                         "Polonnaruwa  : 25");	
+	System.out.println("Hambanthota    : 11"+" "+" "+" "+" "+" "+                                         "Rathnapura   : 26");	
+	System.out.println("Hatton         : 12"+" "+" "+" "+" "+" "+                                         "Trincomalee  : 27");	
+	System.out.println("Kalmunai       : 13"+" "+" "+" "+" "+" "+                                         "Vavuniya     : 28");	
+	System.out.println("Kalutara       : 14"+" "+" "+" "+" "+" "+                                         "Jaffna       : 29");	
+	System.out.println("Kandy          : 15");	
+		
+	System.out.println("");	
+	System.out.println("");
+	System.out.println("     Please enter the corresponding number of the region in order to view the respective area code");	
+	System.out.println("");	
+	System.out.println("");
+	
+	while(true){
+    System.out.println("");	
+	System.out.println("");
+	System.out.print("                                 Please enter the number  : ");
+	int number = scan.nextInt();
+	System.out.println("");	
+	
+	
+	if(number==1){
+		System.out.println("                                  Area code of Ampara is 063");
+		}else if(number==2){
+			System.out.println("                                Area code of Awissawella is 036");
+			}else if(number==3){
+			System.out.println("                                Area code of Anuradhapura is 025");	
+				}else if(number==4){
+			System.out.println("                                Area code of Badulla is 055");		
+					}else if(number==5){
+			System.out.println("                                Area code of Bandarawela is 057");			
+						}else if(number==6){
+			System.out.println("                                Area code of Batticaloa is 065");				
+							}else if(number==7){
+			System.out.println("                                Area code of Chilaw is 032");					
+								}else if(number==8){
+			System.out.println("                                Area code of Colombo is 011");						
+									}else if(number==9){
+			System.out.println("                                Area code of Galle is 091");							
+										}else if(number==10){
+			System.out.println("                                Area code of Gampaha is 033");								
+											}else if(number==11){
+			System.out.println("                                Area code of Hambanthota is 047");									
+												}else if(number==12){
+			System.out.println("                                Area code of Hatton is 051");										
+													}else if(number==13){
+			System.out.println("                                Area code of Kalmunai is 067");											
+														}else if(number==14){
+			System.out.println("                                Area code of Kalutara is 034");												
+															}else if(number==15){
+			System.out.println("                                Area code of Kandy is 081");													
+																}else if(number==16){
+			System.out.println("                                Area code of Kegalle is 035");														
+																	}else if(number==17){
+			System.out.println("                                Area code of Kurunegala is 037");															
+																		}else if(number==18){
+			System.out.println("                                Area code of Mannar is 023");																
+																			}else if(number==19){
+			System.out.println("                                Area code of Matale is 066");																	
+																				}else if(number==20){
+			System.out.println("                                Area code of Matara is 041");																		
+																					}else if(number==21){
+			System.out.println("                                Area code of Nawalapitiya is 054");																			
+																						}else if(number==22){
+			System.out.println("                                Area code of Negombo is 031");																				
+																							}else if(number==23){
+			System.out.println("                                Area code of NuwaraEliya is 052");																					
+																								}else if(number==24){
+			System.out.println("                                Area code of Panadura is 038");																						
+																									}else if(number==25){
+			System.out.println("                                Area code of Polonnaruwa is 027");																							
+																										}else if(number==26){
+			System.out.println("                                Area code of Rathnapura is 045");																								
+																											}else if(number==27){
+			System.out.println("                                Area code of Trincomalee is 026");																									
+																												}else if(number==28){
+			System.out.println("                                Area code of Vavuniya is 024");																										
+																													}else if(number==29){
+			System.out.println("                                Area code of Jaffna is 021");																											
+																														}
+																															
+		}
+		}
+	}
